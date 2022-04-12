9d1149eaf643efc0e3f58e3c63988568882a8e57
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-12 19:14:33 +0000

    deploy: 2c654db810d593dddbdcb53c00e702064fd55337

--- sketch.js
+++ sketch.js
@@ -2,13 +2,13 @@
 
 function setup() {
     createCanvas(600, 400);
-    var mysprite = createSprite(width/2,0,20,20);
-    mysprite.shapeColor = color(0,0,255);
+    var mysprite = createSprite(width / 2, 0, 20, 20);
+    mysprite.shapeColor = color(0, 0, 255);
     mysprite.velocity.y = 2;
 
-  }
+}
 
-  function draw() {
+function draw() {
     background(220);
     drawSprites();
-  }
+}
