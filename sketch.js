70876dc29a7fa11d4e464277f20c782a2baf0cc8
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-12 19:29:13 +0000

    deploy: d03521cc32823b54f34d30d16910ce03d16efcf3

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
 function setup() {
-    createCanvas(600, 400);
-    var mysprite = createSprite(width / 2, 0, 20, 20);
-    mysprite.shapeColor = color(0, 0, 255);
-    mysprite.velocity.y = 2;
+    createCanvas(windowWidth - 20, windowHeight - 20);
 
 }
 
 function draw() {
     background(220);
     drawSprites();
-    
+}
+
+function mousePressed() {
+    var s = createSprite(mouseX,mouseY,30,30);
+    s.velocity.y = mouseY > height / 2 ? -5 : 5;
 }
