7df5b668ef354e6442f619bea021d27e723ae01e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-12 15:12:29 -0400

    I hate this :(

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
 function setup() {
-    createCanvas(600,600);
-}
+    createCanvas(600, 400);
+    var mysprite = createSprite(width/2,0,20,20);
+    mysprite.shapeColor = color(0,0,255);
+    mysprite.velocity.y = 2;
 
-function draw() {
+  }
+
+  function draw() {
     background(220);
-}
+    drawSprites();
+  }
